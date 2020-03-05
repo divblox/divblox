a064f1520c64a40aa18adab66d1171a8d6e74744
DaniS0312 <dani.simeonov@stratusolve.com>
2020-03-05 12:12:59 +0200

    removed alreayd amde components

--- project/components/data_model/category_crud/component.php
+++
@@ -1,9 +0,0 @@
-<?php
-require("../../../../divblox/divblox.php");
-class CategoryCrudController extends ProjectComponentController {
-    public function __construct($ComponentNameStr = 'Component') {
-        parent::__construct($ComponentNameStr);
-    }
-}
-$ComponentObj = new CategoryCrudController("category_crud");
-?>
--- project/components/data_model/ticket_crud/component.php
+++
@@ -1,9 +0,0 @@
-<?php
-require("../../../../divblox/divblox.php");
-class TicketCrudController extends ProjectComponentController {
-    public function __construct($ComponentNameStr = 'Component') {
-        parent::__construct($ComponentNameStr);
-    }
-}
-$ComponentObj = new TicketCrudController("ticket_crud");
-?>
