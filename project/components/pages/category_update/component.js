258c5d53a04170474dfb6ac39b2f14b84e61f60a
DaniS0312 <dani.simeonov@stratusolve.com>
2020-03-18 09:06:28 +0200

    subcategories

--- project/components/pages/category_update/component.js
+++ project/components/pages/category_update/component.js
@@ -3,17 +3,10 @@ if (typeof component_classes['pages_category_update'] === "undefined") {
         constructor(inputs, supports_native, requires_native) {
             super(inputs, supports_native, requires_native);
             // Sub component config start
-            this.sub_component_definitions =
-                [{
-                    "component_load_path": "navigation/side_navbar",
-                    "parent_element": "s8gjT",
-                    "arguments": {"uid": "navigation_side_navbar_1"}
-                },
-                    {
-                        "component_load_path": "data_model/category_crud_update",
-                        "parent_element": "VrKSD",
-                        "arguments": {}
-                    }];
+            this.sub_component_definitions = 
+                [{"component_load_path":"navigation/side_navbar","parent_element":"s8gjT","arguments":{"uid":"navigation_side_navbar_1"}},
+            {"component_load_path":"data_model/category_crud_update","parent_element":"VrKSD","arguments":{"uid":"data_model_category_crud_update_1"}},
+            {"component_load_path":"data_model/subcategory_list","parent_element":"qg1nR","arguments":{}}];
             // Sub component config end
         }
 
@@ -23,15 +16,16 @@ if (typeof component_classes['pages_category_update'] === "undefined") {
             this.updateBreadCrumbs();
         }
 
-        initCustomFunctions() {
+        initCustomFunctions() {        //Custom javascript here
             super.initCustomFunctions();
             getComponentElementById(this, "AllCategories").on("click", function () {
                 loadPageComponent("category_management");
             });
             $(document).on("click", ".category-breadcrumb", function () {
             	let category_id = $(this).attr("id").replace("CategoryId_","");
+            	setGlobalConstrainById("Category", category_id);
             	dxLog("Cat id: "+category_id);
-				//loadPageComponent("category_management");
+				loadPageComponent("category_update");
 			});
         }
 
@@ -45,11 +39,11 @@ if (typeof component_classes['pages_category_update'] === "undefined") {
                     let html = "";
 
                     dxLog("Returned: "+data_obj.ReturnData);
-
+                    // dxLog("ReturnData array length:  " + data_obj.ReturnData.length);
                     let category_keys = Object.keys(data_obj.ReturnData);
                     let count = 1;
                     category_keys.forEach(function(key) {
-                    	if (count == (data_obj.ReturnData.length - 1)) {
+                    	if (count === (category_keys.length)) {
 							html = "<li class=\"breadcrumb-item active\">" + key + "</li>";
 						} else {
 							html = "<li class=\"breadcrumb-item\"><a id=\"CategoryId_" + data_obj.ReturnData[key] + "\" class=\"category-breadcrumb\" href=\"#\">" + key + "</a></li>";
