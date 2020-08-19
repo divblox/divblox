e51c7498feef1d203c31a35e8c755edf43d81055
DaniSimeonov <dani.simeonov@gmail.com>
2020-08-19 11:11:58 +0200

    initial sync and gen and local environment setup

---
+++ divblox/config/framework/environments.php
@@ -0,0 +1,32 @@
+<?php
+$ModuleArray = array(
+    "Main",
+);
+$ModuleColorArray = array(
+    "Main" => "#65a7b6",
+);
+$EnvironmentArray = array(
+    "Development_Default" => array(
+        "APP_NAME" => "divblox_app",
+        "DOCUMENT_ROOT" => "/Applications/MAMP/htdocs",
+        "SUBDIRECTORY" => "/divblox",
+        "MAINTENANCE_PASSWORD" => "123456",
+        "HOST_NAME" => "https://localhost/",
+        "NATIVE_WIDGET_ID" => "com.divblox.app",
+        "NATIVE_APP_NAME" => "divblox",
+        "NATIVE_APP_DISPLAY_NAME" => "divblox",
+        "DATABASES" => array(
+            "Main" => array(
+                'server' => 'localhost',
+                'port' => 3306,
+                'database' => 'basic-training-exercise',
+                'username' => 'root',
+                'password' => 'root',
+                'ssl_cert_path' => ''
+            ),
+        )
+    ),
+
+);
+$dxApiKey = "v80rone3sLzNpSYHi4IG1EFQabJ2fZkuXdMcxRWTgmwt9hOl7PDyCAjqVBU56K";
+?>
